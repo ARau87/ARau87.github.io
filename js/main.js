c4ae83241ea254c3cf1c0545ed37901c9b56707f
ARau87 <andirau@gmx.de>
2018-04-14 13:43:25 +0200

    Improved mobile css and created mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 'use strict';
 
-const menuClick
+// Toggle mobile nav when clicking on the burger button
+const toggleMenu = () => {
 
-$('.burger').click(() => {
-  $('.mobile-nav').removeClass('mobile-nav--hidden');
-});
+  const classes = $('.mobile-nav').attr('class').split(/\s+/);
+
+  classes.indexOf('mobile-nav--hidden') > 0 ? 
+                                              $('.mobile-nav').removeClass('mobile-nav--hidden') 
+                                              : 
+                                              $('.mobile-nav').addClass('mobile-nav--hidden');
+
+}
+
+// Creating a fixed nav when scrolling down
+const fixNavbar = (event) => {
+
+  $(document).scrollTop() > 50 ?
+                            $('.nav-wrapper').addClass('nav-wrapper--fixed')
+                            :
+                            $('.nav-wrapper').removeClass('nav-wrapper--fixed');
+
+
+};
+
+
+// Adding listeners to page elements
+$('.burger-wrapper').click(toggleMenu);
+$('.mobile-nav .nav-list').click(toggleMenu);
+
+$(document).scroll(fixNavbar);
+
